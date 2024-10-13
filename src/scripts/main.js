34a2fd62d5faa3c505d81739bc5aff8955992a47
KlebanAndrii <klebanandrii83@gmail.com>
2024-10-13 21:46:55 +0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,75 @@
+/* fixed header */
+const headerEl = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > headerEl.offsetHeight) {
+    headerEl.classList.add("scrolled");
+  } else {
+    headerEl.classList.remove("scrolled");
+  }
+});
+
+/* simple slider with 2 slides */
+const swiper = new Swiper(".feedback .swiper", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  // loop: true,
+  navigation: {
+    nextEl: ".slider-btn--next",
+    prevEl: ".slider-btn--prev",
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    600: {
+      slidesPerView: 2,
+      centeredSlides: true,
+    }
+  },
+});
+
+
+
+
+/* tabs */
+const showTabs = (tabsNav, tabsContent) => {
+  tabsNav?.forEach((navLink, index) => {
+    navLink?.addEventListener("click", () => {
+      tabsNav.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      navLink.classList.add("active");
+
+      let navIndex = index;
+
+      tabsContent.forEach((contentItem, index) => {
+        contentItem.classList.remove("active");
+
+        if (index === navIndex) {
+          contentItem.classList.add("active");
+        }
+      });
+    });
+  });
+};
+
+const tabsNav = document.querySelectorAll(".bikes .tabs-nav__link");
+const tabsContent = document.querySelectorAll(".bikes .tabs-content");
+showTabs(tabsNav, tabsContent);
+
+
+
+/* burger */
+const burgerBtnEl = document.querySelector(".burger");
+
+const handleBurgerClick = () => {
+  burgerBtnEl.classList.toggle("active");
+};
+
+document.addEventListener("click", (e) => {
+
+  if (e.target.classList.contains("burger")) {
+    handleBurgerClick();
+  }
+})
